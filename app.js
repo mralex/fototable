62211b0ccbb5ec9d8d80afa73893bb92e15b8ebe
Alex Roberts <bse@Tank.local>
2010-08-22 00:50:15 -0700

    Styled header and background

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ $(function() {
 				}
 			}
 			
+			var w = $(document).width() - 300;	
+			var w2 = w/2;
+			console.log(w);
+			
 			var photosUsed = Array();
 			for (var i = 0; i < maxImgs; i++) {
 				do {
@@ -57,8 +61,20 @@ $(function() {
 				//http://farm{farm-id}.static.flickr.com/{server-id}/{id}_{secret}_[mstb].jpg
 				var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_m.jpg");
 				var top = Math.floor(Math.random() * 500);
-				var left = Math.floor(Math.random() * 600); //100 + (Math.random() * 1024);
-				var rotation = Math.floor((Math.random() * -100) + 50);
+				var left = Math.floor(Math.random() * w); //100 + (Math.random() * 1024);
+
+				var rotation;
+				if (left > w2) {
+					// rotate to the right
+					// ((100/500) * 80)
+					// (((left - w2) / w2) * 80)
+					rotation = Math.floor((Math.random() * (((left - w2) / w2) * 80)));
+				} else {
+					// rotate to the left
+					// -(((left/w2) * -80) + 80)
+					rotation = Math.floor((Math.random() * -(((left/w2) * -80) + 80)));
+					
+				}
 				img.attr("style", "position:absolute; top:" + top + "px; left:" + left + "px; -webkit-transform:rotate("+rotation+"deg); border:5px solid rgba(255, 255, 255, 0.8); -webkit-box-shadow:0px 0px 10px rgba(0, 0, 0, 0.5); z-index:" + imgs.length);
 				
 				img[0].item = item;
@@ -71,6 +87,8 @@ $(function() {
 					var _sy = e.pageY;
 					var _ox = parseInt(this.style.left);
 					var _oy = parseInt(this.style.top);
+					
+					this.moved = false;
 					
 					// move selected item to the top of the pile
 					imgs.updateZ(this.style.zIndex);
@@ -85,6 +103,7 @@ $(function() {
 					this.onmousemove = function(e) {
 						this.style.left = (window.scrollX + _ox + e.clientX - _sx + 'px');
 						this.style.top = (window.scrollY + _oy + e.clientY - _sy + 'px');
+						this.moved = true;
 					}
 
 					return false; });
@@ -92,32 +111,60 @@ $(function() {
 				img.mouseup(function(e) { 
 					this.onmousemove = null;
 					this.style.borderColor = "rgba(255, 255, 255, 0.8)";
+					
+					if (!this.moved) {
+						var overlay;
+						var full;
+						if ($("#full").length == 0) {
+							overlay = $("<div/>").attr("id", "overlay");
+							full = $("<div/>");
+							overlay.append(full);
+							$("body").append(overlay);
+							overlay.fadeIn(100);
+						} else {
+							full = $("#full");
+						}
+
+						full.attr("id", "full");
+						full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
+
+						item = this.item;
+
+						var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
+						full.html(img);
+						full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
+
+						overlay.click(function(e) {
+							$("#overlay").fadeOut(200, function() {	$(this).detach(); });
+						})
+					}
+					
 					return false; });
 				
 				img.dblclick(function(e) {
-					var overlay;
-					var full;
-					if ($("#full").length == 0) {
-						overlay = $("<div/>").attr("id", "overlay");
-						full = $("<div/>");
-						overlay.append(full);
-						$("body").append(overlay);
-					} else {
-						full = $("#full");
-					}
-
-					full.attr("id", "full");
-					full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
-					
-					item = this.item;
-					
-					var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
-					full.html(img);
-					full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
-					
-					overlay.click(function(e) {
-						$("#overlay").detach();
-					})
+					// var overlay;
+					// var full;
+					// if ($("#full").length == 0) {
+					// 	overlay = $("<div/>").attr("id", "overlay");
+					// 	full = $("<div/>");
+					// 	overlay.append(full);
+					// 	$("body").append(overlay);
+					// } else {
+					// 	full = $("#full");
+					// }
+					// 
+					// full.attr("id", "full");
+					// full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
+					// 
+					// item = this.item;
+					// 
+					// var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
+					// full.html(img);
+					// full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
+					// 
+					// overlay.click(function(e) {
+					// 	$("#overlay").detach();
+					// })
 					
 					return false;
 				});
