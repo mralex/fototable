922f067028b78392cb93662111e0c1d21371a5ab
Alex Roberts <bse@Tank.local>
2010-08-22 10:58:12 -0700

    Fixed drag and drop - By attaching to the global document handler instead of per image, image would lose focus if mouse left bounds

--- app.js
+++ app.js
@@ -7,7 +7,30 @@ var xM = 0;
 var yM = 0;
 var realx = 0;
 var realy = 0;
+var imgs = [];
+
 $(function() {
+	imgs.maxZ = function() {
+		var z = 0;
+		for (var i = 0; i < this.length; i++) {
+			var curZ = parseInt(this[i][0].style.zIndex);
+			if (curZ > z) z = curZ;
+		}
+		return z;
+	}
+	imgs.updateZ = function(originZ) {
+		if (originZ == this.maxZ()) return;
+		
+		for (var i = 0; i < this.length; i++) {
+			var curZ = parseInt(this[i][0].style.zIndex);
+			
+			if (curZ >= originZ) {
+				var z = curZ;
+				this[i][0].style.zIndex = z--;
+			}
+		}
+	}
+	
 	$("#sF").submit(function() {
 		// alert(e.value);
 		if ($("#s").val() == "") { 
@@ -23,27 +46,6 @@ $(function() {
 			$("#results").html("Loaded! ("+ data.photos.photo.length +")<br/>");
 			response = data;
 			var len = data.photos.photo.length;
-			var imgs = [];
-			imgs.maxZ = function() {
-				var z = 0;
-				for (var i = 0; i < this.length; i++) {
-					var curZ = parseInt(this[i][0].style.zIndex);
-					if (curZ > z) z = curZ;
-				}
-				return z;
-			}
-			imgs.updateZ = function(originZ) {
-				if (originZ == this.maxZ()) return;
-				
-				for (var i = 0; i < this.length; i++) {
-					var curZ = parseInt(this[i][0].style.zIndex);
-					
-					if (curZ >= originZ) {
-						var z = curZ;
-						this[i][0].style.zIndex = z--;
-					}
-				}
-			}
 			
 			var w = $(document).width() - 300;	
 			var w2 = w/2;
@@ -76,99 +78,14 @@ $(function() {
 					
 				}
 				img.attr("style", "position:absolute; top:" + top + "px; left:" + left + "px; -webkit-transform:rotate("+rotation+"deg); border:5px solid rgba(255, 255, 255, 0.8); -webkit-box-shadow:0px 0px 10px rgba(0, 0, 0, 0.5); z-index:" + imgs.length);
+				img.addClass("photo");
 				
 				img[0].item = item;
 				
-				img.mouseout(function(e) {
-					this.onmousemove = null;
-				})
-				img.mousedown(function(e) { 
-					var _sx = e.pageX;
-					var _sy = e.pageY;
-					var _ox = parseInt(this.style.left);
-					var _oy = parseInt(this.style.top);
-					
-					this.moved = false;
-					
-					// move selected item to the top of the pile
-					imgs.updateZ(this.style.zIndex);
-					this.style.zIndex = imgs.maxZ() + 1;
-					
-					this.style.borderColor = "rgba(0, 0, 255, 0.8)";
-							
-					document.body.focus();
-					document.onselectstart = function() { return false; };
-					this.ondragstart = function() { return false; };
-					
-					this.onmousemove = function(e) {
-						this.style.left = (window.scrollX + _ox + e.clientX - _sx + 'px');
-						this.style.top = (window.scrollY + _oy + e.clientY - _sy + 'px');
-						this.moved = true;
-					}
+				// img.mouseout(function(e) {
+				// 	this.onmousemove = null;
+				// })
 
-					return false; });
-
-				img.mouseup(function(e) { 
-					this.onmousemove = null;
-					this.style.borderColor = "rgba(255, 255, 255, 0.8)";
-					
-					if (!this.moved) {
-						var overlay;
-						var full;
-						if ($("#full").length == 0) {
-							overlay = $("<div/>").attr("id", "overlay");
-							full = $("<div/>");
-							overlay.append(full);
-							$("body").append(overlay);
-							overlay.fadeIn(100);
-						} else {
-							full = $("#full");
-						}
-
-						full.attr("id", "full");
-						full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
-
-						item = this.item;
-
-						var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
-						full.html(img);
-						full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
-
-						overlay.click(function(e) {
-							$("#overlay").fadeOut(200, function() {	$(this).detach(); });
-						})
-					}
-					
-					return false; });
-				
-				img.dblclick(function(e) {
-					// var overlay;
-					// var full;
-					// if ($("#full").length == 0) {
-					// 	overlay = $("<div/>").attr("id", "overlay");
-					// 	full = $("<div/>");
-					// 	overlay.append(full);
-					// 	$("body").append(overlay);
-					// } else {
-					// 	full = $("#full");
-					// }
-					// 
-					// full.attr("id", "full");
-					// full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
-					// 
-					// item = this.item;
-					// 
-					// var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
-					// full.html(img);
-					// full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
-					// 
-					// overlay.click(function(e) {
-					// 	$("#overlay").detach();
-					// })
-					
-					return false;
-				});
-				
 				imgs.push(img);
 	            img.appendTo("#results");
 			}
@@ -176,6 +93,76 @@ $(function() {
 		
 		return false;
 	});
+	
+	// Drag and drop
+	document.onmousedown = function(e) { 
+		var t = e.target;
+		if (!$(t).hasClass("photo")) return;
+		
+		var _sx = e.pageX;
+		var _sy = e.pageY;
+		var _ox = parseInt(t.style.left);
+		var _oy = parseInt(t.style.top);
+		
+		t.moved = false;
+		
+		// move selected item to the top of the pile
+		imgs.updateZ(t.style.zIndex);
+		t.style.zIndex = imgs.maxZ() + 1;
+		
+		t.style.borderColor = "rgba(0, 0, 255, 0.8)";
+		
+		document.onmousemove = function(e) {
+			t.style.left = (window.scrollX + _ox + e.clientX - _sx + 'px');
+			t.style.top = (window.scrollY + _oy + e.clientY - _sy + 'px');
+			t.moved = true;
+		}
+		
+		document.body.focus();
+		document.onselectstart = function() { return false; };
+		t.ondragstart = function() { return false; };
+
+		return false; 
+	};
+
+	document.onmouseup = function(e) { 
+		var t = e.target;
+		if (!$(t).hasClass("photo")) return;
+		
+		document.onmousemove = null;
+		document.onselectstart = null;
+		t.ondragstart = null;
+		t.style.borderColor = "rgba(255, 255, 255, 0.8)";
+		
+		if (!t.moved) {
+			var overlay;
+			var full;
+			if ($("#full").length == 0) {
+				overlay = $("<div/>").attr("id", "overlay");
+				full = $("<div/>");
+				overlay.append(full);
+				$("body").append(overlay);
+				overlay.fadeIn(100);
+			} else {
+				full = $("#full");
+			}
+
+			full.attr("id", "full");
+			full.attr("style", "position: absolute; background-color: #fff; width: 500px; z-index:10000; -webkit-box-shadow: 0 6px 10px #111; border: 5px solid #fff; left:" + ((document.body.clientWidth / 2) - 250) + "px; top: 100px;");
+
+			item = t.item;
+
+			var img = $("<img/>").attr("src", "http://farm"+ item.farm +".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg");
+			full.html(img);
+			full.append("<h2><a href='http://flickr.com/photos/"+item.owner +"/"+item.id+"'>" + item.title + "</a></h2>");
+
+			overlay.click(function(e) {
+				$("#overlay").fadeOut(200, function() {	$(this).detach(); });
+			})
+		}
+		
+		return false; 
+	};
 	
 	savedSearch = localStorage.getItem('savedSearch');
 	if (savedSearch != null) {
